d2e15e0ac524f613b8f616d96b303aac64eb3b23
Gloryxx <kaniagalih28@gmail.com>
2023-12-17 16:23:05 +0700

    update all

--- app.py
+++ app.py
@@ -3,8 +3,13 @@ from keras.models import load_model
 from keras.preprocessing import image
 import keras.utils as image
 import numpy as np
+import os
 
 app = Flask(__name__)
+
+upload_folder = os.path.join('static', 'uploads')
+
+app.config['UPLOAD'] = upload_folder
 
 dic = {0 : 'Normal', 1 : 'Pneumonia'}
 
@@ -18,7 +23,8 @@ def predict_label(img_path):
 	i = i.reshape(1, 224,224,3)
 	p = model.predict(i)
 	predict_index = np.argmax(p)
-	return dic[predict_index]
+	confidence_score = np.max(p) * 100
+	return dic[predict_index], confidence_score
 
 
 # routes
@@ -33,14 +39,12 @@ def about_page():
 @app.route("/submit", methods = ['GET', 'POST'])
 def get_output():
 	if request.method == 'POST':
-		img = request.files['my_image']
+		file = request.files['my_image']
+		file.save(os.path.join(app.config['UPLOAD'], file.filename))
+		img = os.path.join(app.config['UPLOAD'], file.filename)
+		p = predict_label(img)
 
-		img_path = "static" + img.filename	
-		img.save(img_path)
-
-		p = predict_label(img_path)
-
-	return render_template("index.html", prediction = p, img_path = img_path)
+	return render_template("index.html", prediction = p, confidence = p, img_path = img)
 
 
 if __name__ =='__main__':
